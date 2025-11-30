0e6b5d64b1e83a748fb0dafcefb8eae16f168908
tuokaikyle <kaituo@outlook.com>
2025-12-01 01:34:42 +1000

    Should be typed

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,13 @@
+export type Version = {
+  version?: string;
+  names: string[];
+  year?: number | undefined;
+  chapterCount: number;
+  chapters: number[];
+  zhipiCount?: number;
+  description: { source: string; content: string[] }[];
+};
+
 // To group numbers into rows of max 10 numbers or when non-consecutive
 // Useful when display chapter numbers
 export const groupNumbers = (arr: number[]) => {
