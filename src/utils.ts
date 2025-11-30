846ba16995ead81a940b6c61029b5f7a0e773407
tuokaikyle <kai_tuo@outlook.com>
2025-12-01 01:28:16 +1000

    Fetch data

---
+++ src/utils.ts
@@ -0,0 +1,26 @@
+// To group numbers into rows of max 10 numbers or when non-consecutive
+// Useful when display chapter numbers
+export const groupNumbers = (arr: number[]) => {
+  const rows = [];
+  let currentRow = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    currentRow.push(arr[i]);
+
+    const shouldBreak = 
+      currentRow.length === 10 || 
+      (i < arr.length - 1 && arr[i] !== arr[i + 1] - 1);
+
+    if (shouldBreak) {
+      rows.push([...currentRow]);
+      currentRow = [];
+    }
+  }
+
+  if (currentRow.length > 0) {
+    rows.push(currentRow);
+  }
+
+  return rows;
+};
+
